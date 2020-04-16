b6b97d13dae4910ad66102c25f732b0583acf0b8
Michael Sampson2 <msampson2@Michael2020macbook.local>
2020-04-16 08:10:16 -0400

    stopping point

---
+++ walkdatabase/src/main/java/com/practice/walks/WalkKeeperApplication.java
@@ -0,0 +1,15 @@
+package com.practice.walks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkKeeperApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WalkKeeperApplication.class, args);
+
+    }
+}
